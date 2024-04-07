0f0772f9ed2441bed16a19620d3d642bfb838741
Adam Karasek <adamkarasek2402@gmail.com>
2024-04-07 13:29:28 +0200

    Added one important cout.

--- poker-game.cpp
+++ poker-game.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "Dupa"; // testing how git repo push works
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
