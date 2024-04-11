863e3dadb50cecb58c247a40c6e0b302eb0aecce
Dym3kk <jasdym@gmail.com>
2024-04-11 18:48:51 +0200

    Initial Commit

---
+++ poker-game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  poker-game
+//
+//  Created by Jan Dymkowski on 11/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
