6ce9dcd504946d07734091106c68e9f2f76586c2
Dym3kk <jasdym@gmail.com>
2024-04-25 11:02:56 +0200

    1st version of Deck.h

--- Deck.h
+++ Deck.h
@@ -1,5 +1,20 @@
 #pragma once
-class Deck
-{
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include "Card.h"
+
+class Deck {
+private:
+    std::vector<Card> cards;
+
+public:
+    Deck();
+
+    void shuffle();
+    Card drawCard();
+    void display();
 };
 
+#endif // DECK_H
