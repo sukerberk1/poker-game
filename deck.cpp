52cacffb1c59f0e1b3b8594985e9c065588a5677
Dym3kk <jasdym@gmail.com>
2024-04-25 11:21:38 +0200

    1st version of Deck.cpp

--- deck.cpp
+++ deck.cpp
@@ -1,2 +1,45 @@
-
 #include "Deck.h"
+#include <iostream>
+#include <algorithm>
+
+Deck::Deck() {
+    // Tworzenie talii 52 kart
+    for (int power = 2; power <= 14; ++power) {
+        for (int colorIndex = 0; colorIndex < 4; ++colorIndex) {
+            char color = Card::validColors[colorIndex];
+            Card card(power, color);
+            cards.push_back(card);
+        }
+    }
+}
+
+void Deck::shuffle() {
+    std::random_shuffle(cards.begin(), cards.end());
+}
+
+Card Deck::drawCard() {
+    Card drawnCard = cards.back();
+    cards.pop_back();
+    return drawnCard;
+}
+
+void Deck::display() {
+    for (const Card& card : cards) {
+        card.displayDebug();
+    }
+}
+
+int main() {
+    Deck deck;
+    std::cout << "Shuffling the deck..." << std::endl;
+    deck.shuffle();
+
+    std::cout << "Drawing and displaying 5 cards:" << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        Card drawnCard = deck.drawCard();
+        std::cout << "Card " << i+1 << ": ";
+        drawnCard.displayDebug();
+    }
+
+    return 0;
+}
