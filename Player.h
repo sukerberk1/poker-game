b11e3dfabb08e9e773df37f2e1ef564f3e434f09
Adam Karasek <adamkarasek2402@gmail.com>
2024-05-10 14:32:39 +0200

    First version of player added

--- Player.h
+++ Player.h
@@ -1,5 +1,17 @@
 #pragma once
+#ifndef PLAYER
+#include <iostream>
+#include "Deck.h"
 class Player
 {
+public:
+	int cash;
+	struct {
+		std::string firstCard;
+		std::string secondCard;
+	} hand;
 };
+#endif // !1
+
+
 
--- player.cpp
+++ player.cpp
@@ -1,2 +1,12 @@
-
 #include "Player.h"
+#include "deck.cpp"
+#include <iostream>
+
+int main() {
+	Player player;
+	player.cash = 10000; //to change for some keyboard input, but later
+	player.hand.firstCard = ; //giving cards to player's hand
+	player.hand.secondCard = ;
+	std::cout << "Your cards: " << player.hand.firstCard <<" " << player.hand.secondCard << std::endl;
+	return 0;
+};
