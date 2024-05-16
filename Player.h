596a51c5e15b0e92f02a2ab70db7b61cd2196734
sukerberk1 <szymon.l3232@gmail.com>
2024-05-16 12:58:45 +0200

    Redo Player

--- Player.h
+++ Player.h
@@ -6,22 +6,24 @@
 class Player
 {
 public:
-	Player(int cash);
-	/*struct {
-		std::string firstCard;
-		std::string secondCard;
-	} hand;*/
-	
-	//int hand[2] = {firstCard, secondCard};
-	void addCash(unsigned int x); //funkcja dodaj¹ca pieni¹dze do konta gracza - x - pula aktualnej rozgrywki
-	int subsractCash(unsigned int y); //funkcja odejmuj¹ca pieni¹dze z konta gracza - y - wysokoœæ betu
-	void giveCards(Card c1, Card c2);
-	void resetHand();
+	Player(unsigned int cash);
+	/* Adds cash for the player */
+	void addCash(unsigned int x);
+	/* Takes cash away from the player */
+	int subtractCash(unsigned int y);
+	/* Adds a card to the player's hand. If hand is already fully set (hand contains 2 cards), does nothing. */
+	void giveCard(Card card);
+	/* Resets cards of a given player. Internally sets the underying pointer to the new array of 2 card placeholders */
+	void resetCards();
+	/* 
+	Returns a pointer to the copy of the hand array. Note that it points to the copy, 
+	therefore modification of the returned array does not affect the player
+	*/
 	Card* getCards();
 	
 private:
-	Card hand[2];
-	int cash;
+	Card* hand;
+	unsigned int cash;
 };
 #endif // !PLAYER
 
--- player.cpp
+++ player.cpp
@@ -2,42 +2,36 @@
 #include <iostream>
 
 
-Player::Player(int cash) {
-	if (cash >= 0) {
-		this->cash = cash;
-	}
-	else {
-		throw;
-	}
+Player::Player(unsigned int cash) : cash(cash) {
+	this->hand = new Card[2]{ Card::PLACEHOLDER(), Card::PLACEHOLDER() };
 }
 
 void Player::addCash(unsigned int winningPot) {
 	this -> cash +=  winningPot; //adding money to player's cash from winning pot (winning pot in round)
 }
-int Player::subsractCash(unsigned int bet) {
+int Player::subtractCash(unsigned int bet) {
 	return cash -= bet; //betting in round, bet variable is in round
 }
 
-Card Deck::drawCard() {
-	Card drawnCard = cards.back();
-	cards.pop_back();
-	return drawnCard;
-}
-
 Card* Player::getCards() {
-	Card card1 = Deck::drawCard();
-	Card card2 = Deck::drawCard();
-	Player::giveCards(card1, card2);
+	Card* handCopy = new Card[2]{ hand[0], hand[1] };
+	return handCopy;
 }
 
-void Player::giveCards(Card c1, Card c2) {
-	Player::hand[0] = {c1};
-	Player::hand[1] = {c2};
+void Player::giveCard(Card card) {
+	if (Card::isPlaceholder(hand[0])) {
+		hand[0] = card;
+		return;
+	}
+	if (Card::isPlaceholder(hand[1])) {
+		hand[1] = card;
+		return;
+	}
+	// Loop could be utilized here, but for readability it is left as is.
 }
 
-void Player::resetHand() {
-	Player::hand[0] = {0, 0};
-	Player::hand[1] = {0, 0};
+void Player::resetCards() {
+	hand = new Card[2]{ Card::PLACEHOLDER(), Card::PLACEHOLDER() };
 }
 
 
